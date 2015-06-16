d4b87ac2d43954d47ec7d0076c785c24c2c4dfc6
Vasil Vasilev <vasilvasilev093@gmail.com>
2015-06-16 22:27:24 +0300

    Merge sort improved

--- week1/Tuesday/mergeSort.cpp
+++ week1/Tuesday/mergeSort.cpp
@@ -12,54 +12,83 @@ void print(int* arr, int size)
 	cout << endl;
 }
 
-void merge(int* arr, int* copyArr, int start, int mid, int end)
+
+int max(int x, int y)
 {
-	int arrIndex = start;
-	int left = start;
-	int right = mid + 1;
-
-	while (left <= mid && right <= end)
-	{
-		if (arr[left] <= arr[right]) copyArr[arrIndex++] = arr[left++];
-		else copyArr[arrIndex++] = arr[right++];
-	}
-
-	if (left <= mid)
-	{
-		while (left <= mid) copyArr[arrIndex++] = arr[left++];
-	}
-
-	else while (right <= end) copyArr[arrIndex++] = arr[right++];
-
-	for (int i = 0; i <= end; i++)
-	{
-		arr[i] = copyArr[i];
-	}
+    if(x > y)
+    {
+        return x;
+    }
+    else
+    {
+        return y;
+    }
 }
 
-
-void mergeSort(int* arr, int* copyArr, int start, int end)
+void merge_helper(int *arr, int left, int right, int *copyArr)
 {
-	int mid;
 
-	if (start < end)
-	{
-		mid = (start + end) / 2;
+    if(right == left + 1)
+    {
+        return;
+    }
+    else
+    {
+        int i = 0;
+        int length = right - left;
+        int midpoint_distance = length/2;
+ 
+        int l = left, r = left + midpoint_distance;
 
-		mergeSort(arr, copyArr, start, mid);
-		mergeSort(arr, copyArr, mid + 1, end);
 
-		merge(arr, copyArr, start, mid, end);
-	}
+        merge_helper(arr, left, left + midpoint_distance, copyArr);
+        merge_helper(arr, left + midpoint_distance, right, copyArr);
+
+
+        for(i = 0; i < length; i++)
+        {
+            if(l < left + midpoint_distance && 
+                    (r == right || max(arr[l], arr[r]) == arr[r]))
+            {
+                copyArr[i] = arr[l];
+                l++;
+            }
+            else
+            {
+                copyArr[i] = arr[r];
+                r++;
+            }
+        }
+
+        for(i = left; i < right; i++)
+        {
+            arr[i] = copyArr[i - left];
+        }
+    }
 }
+
+int mergesort(int *arr, int size)
+{
+    int *copyArr = new int[size];
+    if(copyArr != NULL)
+    {
+        merge_helper(arr, 0, size, copyArr);
+        delete [] copyArr;
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
 
 int main()
 {
 
 	int arr[10] = { 5, 1, 9, 4, 7, 2, 3, 5, 6, 8 };
-	int copyArr[10];
 
-	mergeSort(arr, copyArr, 0, 9);
+	mergesort(arr, 10);
 
 	print(arr, 10);
 
